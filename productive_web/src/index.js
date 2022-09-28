9a5704e089892493fe90ed9e65c9a18967690999
Trinidaddy <trinidad961@gmail.com>
2022-09-27 19:22:03 -0500

    Initialize Firebase app

--- productive_web/src/index.js
+++ productive_web/src/index.js
@@ -11,18 +11,18 @@ import { getAuth, connectAuthEmulator } from "firebase/auth";
 import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
 
 // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAsJn0ewPAipnHWexFvb6Nlz8dvUI8JmlQ",
-//   authDomain: "productive-c3d8a.firebaseapp.com",
-//   projectId: "productive-c3d8a",
-//   storageBucket: "productive-c3d8a.appspot.com",
-//   messagingSenderId: "59050319106",
-//   appId: "1:59050319106:web:f844e101dfbd38cdb5761b",
-//   measurementId: "G-N0GZ76G9KH",
-// };
+const firebaseConfig = {
+  apiKey: "AIzaSyAsJn0ewPAipnHWexFvb6Nlz8dvUI8JmlQ",
+  authDomain: "productive-c3d8a.firebaseapp.com",
+  projectId: "productive-c3d8a",
+  storageBucket: "productive-c3d8a.appspot.com",
+  messagingSenderId: "59050319106",
+  appId: "1:59050319106:web:f844e101dfbd38cdb5761b",
+  measurementId: "G-N0GZ76G9KH",
+};
 
 // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
 
 // Auth
